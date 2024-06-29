efce4fa994fd4a9eca0e251e826cc6a2f87643ae
jacky2252v <112156221+jacky2252v@users.noreply.github.com>
2024-06-29 17:40:25 +0530

    new2

--- api/webhook.js
+++ api/webhook.js
@@ -2,30 +2,6 @@ import express from "express";
 import axios from "axios";
 
 export default function handler() {
-    //     const VERIFY_TOKEN = 'my_verify_token';
-
-    //     if (req.method === 'POST') {
-    //         const mode = req.query['hub.mode'];
-    //         const token = req.query['hub.verify_token'];
-    //         const challenge = req.query['hub.challenge'];
-
-    //         if (mode && token === VERIFY_TOKEN) {
-    //             res.status(200).send(challenge);
-    //         } else {
-    //             res.status(403).send('Forbidden');
-    //         }
-    //     } else if (req.method === 'GET') {
-    //         // Handle the incoming webhook request
-    //         const body = req.body;
-
-    //         // Perform your processing here
-    //         console.log('Webhook received:', body);
-
-    //         // Send a response back to WhatsApp API
-    //         res.status(200).send('Webhook received');
-    //     } else {
-    //         res.status(405).send('Method Not Allowed');
-    //     }
 
     const app = express();
     app.use(express.json());
@@ -104,3 +80,29 @@ Checkout README.md to start.</pre>`);
         console.log(`Server is listening on port: ${PORT}`);
     });
 }
+
+
+//     const VERIFY_TOKEN = 'my_verify_token';
+
+//     if (req.method === 'POST') {
+//         const mode = req.query['hub.mode'];
+//         const token = req.query['hub.verify_token'];
+//         const challenge = req.query['hub.challenge'];
+
+//         if (mode && token === VERIFY_TOKEN) {
+//             res.status(200).send(challenge);
+//         } else {
+//             res.status(403).send('Forbidden');
+//         }
+//     } else if (req.method === 'GET') {
+//         // Handle the incoming webhook request
+//         const body = req.body;
+
+//         // Perform your processing here
+//         console.log('Webhook received:', body);
+
+//         // Send a response back to WhatsApp API
+//         res.status(200).send('Webhook received');
+//     } else {
+//         res.status(405).send('Method Not Allowed');
+//     }
